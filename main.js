856b6f0b34a67540a68241245bc846e081c3bf22
Raag Agrawal <ra2816@columbia.edu>
2023-09-10 21:40:01 -0700

    initial website

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sampleButton = document.getElementById('sample-button');
+    
+    sampleButton.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
